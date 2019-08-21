4b0a0c05e432fbaf29ad322c20aaf5f9c93eb5db
alexandermagee <alexandermagee1@gmail.com>
2019-08-21 18:32:38 +0100

    Adding the quotes object with a random quote generator method

---
+++ src/components/quotes.js
@@ -0,0 +1,19 @@
+export const quotes = {
+    quotes: {
+        "Sir Richard Branson" : "To me, business isn’t about wearing suits or pleasing stockholders. It’s about being true to yourself, your ideas and focusing on the essentials.",
+        "Tim Cook" : "Life is fragile. We’re not guaranteed a tomorrow so give it everything you’ve got.",
+        "Tom Kelley" : "Fail often so you can succeed sooner.",
+        "Israelmore Ayivor" : "We are no longer in the dispensation of age and experience. We are in the era of knowledge and information. Information leads a true leader and a true leader leads others." 
+
+    },
+  get randomQuote () {
+    const authors = Object.keys(this.quotes);
+    let randomIndex = authors[Math.floor(Math.random()*authors.length)];
+    return {
+      author: randomIndex,
+      text: this.quotes[randomIndex]
+    }
+  }
+}
+
+
